5ef4da8425502c2691c05144049c0d96a93ae8ca
Archy Chim <archibaldo2001@gmail.com>
2024-11-05 19:34:43 -0600

    video
    vidoe added

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,27 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import './App.css';
 import background from './assets/img/background.jpg'
+import video1 from './assets/video/video.mp4'
+
 
 function App() {
 
   return (
-  
+    <>
       <div className='header-container'>
         <Navbar />
         <img src={background} alt="" className='header-image'/>
         <div className='header-text'>
           <h1>Echos From Beyond</h1> <br />
-          <p>OnlyFans.com/ArchyChim</p> <br />
           <p>Yamete Nyaaaa</p>
         </div>
       </div>
+
+      <div className='video-container'>
+        <video src={video1} autoPlay loop muted className='video-background'></video>
+      </div>
       
-    
+    </>
   );
 }
 
