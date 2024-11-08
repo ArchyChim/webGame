cd5b95c33735e3d981f25210d8149c4df9acb533
Archy Chim <archibaldo2001@gmail.com>
2024-11-07 18:12:34 -0600

    video text
    video text added

--- src/App.jsx
+++ src/App.jsx
@@ -14,12 +14,16 @@ function App() {
         <img src={background} alt="" className='header-image'/>
         <div className='header-text'>
           <h1>Echos From Beyond</h1> <br />
-          <p>Yamete Nyaaaa</p>
+          <p>Yamete Kudasai</p>
         </div>
       </div>
 
       <div className='video-container'>
         <video src={video1} autoPlay loop muted className='video-background'></video>
+        <div className='text-video'>
+          <h1>Experiencia de Horror Única</h1>
+          <p>Sumerjete en esta experiencia única</p>
+        </div>
       </div>
       
     </>
