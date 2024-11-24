2744338edc4437cb1217419c45e3d9f5d324060e
Archy Chim <archibaldo2001@gmail.com>
2024-11-23 19:22:49 -0600

    changes
    afjkjsdlkfj

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,15 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import './App.css';
 import video1 from './assets/video/video.mp4'
-import image1 from './assets/img/image.jpg'
+
+
 
 
 function App() {
 
   return (
     <>
-
+    
         <div id='home' className='header-container'>
           <Navbar />
           <div className='header-text'>
@@ -36,17 +37,7 @@ function App() {
           </div>
         </div>
       
-
-      <div id='history' className="history-section">
-        <img src={image1} alt="" className="image1" />
-        <div className="history-text">
-          <p>Ethan Blackwood, es un detective que lleva años atormentado por la desaparición de su hermana menor, 
-            Alice, quien desapareció sin dejar rastro hace cinco años. La investigación no arrojó ningún resultado, 
-            y la familia Blackwood se vio sumida en un profundo dolor. Ethan, incapaz de dejar atrás el misterio, se obsesionó con encontrar la verdad, 
-            investigando por su cuenta cada pista, cada rumor, cada sombra que pudiera conducirlo al paradero de Alice.</p>
-        </div>
-
-      </div>
+      
       
     </>
   );
