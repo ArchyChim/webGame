546f7cbdbd6dae63c5480f75710197e2d6f7a55f
Archy Chim <archibaldo2001@gmail.com>
2024-11-23 20:10:35 -0600

    Components
    Components Added (Home & History)

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import Navbar from './components/Navbar'
 import './App.css';
-import video1 from './assets/video/video.mp4'
+import Navbar from './components/Navbar'
+import HomeSection from './components/HomeSection';
 import HistorySection from './components/HistorySection';
 
 
@@ -11,32 +11,9 @@ function App() {
 
   return (
     <>
+      <Navbar />
     
-        <div id='home' className='header-container'>
-          <Navbar />
-          <div className='header-text'>
-            <h1>Echos From Beyond</h1> <br />
-          </div>
-          <div className="arrow">
-            <span></span>
-            <span></span>
-          </div>  
-        </div>
-
-        <div className="banner">
-          <div className="banner-text">
-            <h2>La mejor experiencia de Horror</h2> <br />
-            <p>¡Descúbrelo!</p>
-          </div>
-        </div>
-
-        <div className='video-container'>
-          <video src={video1} autoPlay loop muted className='video-background'></video>
-          <div className='text-video'>
-            <h1>Experiencia de Horror Única</h1> <br />
-            <p>¡Adéntrate al horror psicológico!</p>
-          </div>
-        </div>
+      <HomeSection/>
       
       <HistorySection />
       
---
+++ src/components/HomeSection.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import video1 from '../assets/video/video.mp4'
+
+
+function HomeSection () {
+    return (
+        <>
+        <div id='home' className='header-container'>
+            <div className='header-text'>
+                <h1>Echos From Beyond</h1> <br />
+            </div>
+            <div className="arrow">
+                <span></span>
+                <span></span>
+            </div>
+        </div>
+
+        <div className="banner">
+            <div className="banner-text">
+                <h2>La mejor experiencia de Horror</h2> <br />
+                <p>¡Descúbrelo!</p>
+            </div>
+        </div>
+
+        <div className='video-container'>
+            <video src={video1} autoPlay loop muted className='video-background'></video>
+            <div className='text-video'>
+                <h1>Experiencia de Horror Única</h1> <br />
+                <p>¡Adéntrate al horror psicológico!</p>
+            </div>
+        </div></>
+    );
+}
+
+export default HomeSection;
