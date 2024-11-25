65b8dfc364ca3453dfabb5e0b03ef434ffb9e408
Archy Chim <archibaldo2001@gmail.com>
2024-11-25 14:13:09 -0600

    first responsive
    first responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
 import './navbar.css';
 
 function Navbar(){
+
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
     return (
         <nav className="navbar">
-            <a href="#home">Inicio</a>
-            <a href="#history">Historia</a>
-            <a href="#gallery">Galeria</a>
-            <a href="#download">Descargar</a>
-            <a href="#contact">Contacto</a>             
+            <button className="menu-toggle" onClick={toggleMenu}>
+                <span className="bar"></span>
+                <span className="bar"></span>
+                <span className="bar"></span>
+            </button>
+            <div className={`nav-links ${isMenuOpen ? "open" : ""}`}>
+                <a href="#home">Inicio</a>
+                <a href="#history">Historia</a>
+                <a href="#gallery">Galeria</a>
+                <a href="#download">Descargar</a>
+                <a href="#contact">Contacto</a> 
+            </div>
+                        
         </nav>
 
     );
