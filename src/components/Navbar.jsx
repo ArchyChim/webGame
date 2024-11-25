eb51d3d1ad76210ec8c5fef15d85aa6732994a70
Archy Chim <archibaldo2001@gmail.com>
2024-11-25 12:54:40 -0600

    akjsdhf
    asdfasdf

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,13 @@
 import React from "react";
 
-
 function Navbar(){
     return (
         <nav className="navbar">
-                <a href="#home">Inicio</a>
-                <a href="#history">Historia</a>
-                <a href="#gallery">Galeria</a>
-                <a href="#download">Descargar</a>
-                <a href="#contact">Contacto</a>
-            
+            <a href="#home">Inicio</a>
+            <a href="#history">Historia</a>
+            <a href="#gallery">Galeria</a>
+            <a href="#download">Descargar</a>
+            <a href="#contact">Contacto</a>             
         </nav>
 
     );
