ee17b4c039a2eeeccb06ba2c11f177901b961114
Archy Chim <archibaldo2001@gmail.com>
2024-11-27 18:01:00 -0600

    llllll
    installer link updated

--- src/components/DwloadSection.jsx
+++ src/components/DwloadSection.jsx
@@ -15,7 +15,7 @@ function DwloadSection () {
             <ul>
 
                 <li>
-                    <a href = "https://drive.google.com/file/d/1nC0jE46M0Hghjp-eHAOd_CoYW6Nv83-J/view?usp=sharing" download className="download windows">
+                    <a href = "https://www.dropbox.com/scl/fi/wnentn2279fseu9fwtxbf/EchoesFromBeyond.exe?rlkey=fm8me9gsv3uwmmdhqjjve7hko&st=qn087y0o&dl=1" download className="download windows">
                         <i className='bx bxl-windows'></i>
                         <div className="text-icon-container">
                             <span className="df">Descargar para</span>
@@ -25,7 +25,7 @@ function DwloadSection () {
                 </li>
 
                 <li>
-                    <a href = "https://drive.google.com/file/d/1nC0jE46M0Hghjp-eHAOd_CoYW6Nv83-J/view?usp=sharing" download className="download apple">
+                    <a href = "https://www.dropbox.com/scl/fi/wnentn2279fseu9fwtxbf/EchoesFromBeyond.exe?rlkey=fm8me9gsv3uwmmdhqjjve7hko&st=qn087y0o&dl=1" download className="download apple">
                         <i className='bx bxl-apple'></i>
                         <div className="text-icon-container">
                             <span className="df">Descargar para</span>
